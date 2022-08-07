234819c64d4c7a68e6e83cad12855f1a08538141
manemis <manemis@gmail.com>
2022-08-07 17:50:38 +0900

    라우터 만듬

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import About from "./routes/About";
 import Home from "./routes/Home";
-import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <Home />;
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
 
 export default App;
