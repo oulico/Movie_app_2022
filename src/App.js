863ac2fb9d7d1261678f019ae888f91f42bab997
manemis <manemis@gmail.com>
2022-08-07 18:29:56 +0900

    end

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { HashRouter, Route } from "react-router-dom";
 import About from "./routes/About";
 import Home from "./routes/Home";
 import Navigation from "./components/Navigation";
+import Detail from "./routes/Detail";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
+      <Route path="/movie-detail" component={Detail} />
     </HashRouter>
   );
 }
